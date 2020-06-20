6568643b763fd954d64e14f6cbfd90f2ceb7f660
Graitavicius <grait.mindaugas@gmail.com>
2020-06-20 23:12:49 +0300

    Adding bootstrap, angular materials, creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmployeeComponent } from './employee/employee.component';
+import { ShowEmpComponent } from './employee/show-emp/show-emp.component';
+import { EditEmpComponent } from './employee/edit-emp/edit-emp.component';
+import { AddEmpComponent } from './employee/add-emp/add-emp.component';
+import { DepartmentComponent } from './department/department.component';
+import { ShowDepComponent } from './department/show-dep/show-dep.component';
+import { EditDepComponent } from './department/edit-dep/edit-dep.component';
+import { AddDepComponent } from './department/add-dep/add-dep.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    ShowEmpComponent,
+    EditEmpComponent,
+    AddEmpComponent,
+    DepartmentComponent,
+    ShowDepComponent,
+    EditDepComponent,
+    AddDepComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
